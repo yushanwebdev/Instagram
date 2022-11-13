c0c67b301b36f91f8522bc0cacd66e5b1f7164ed
yushanwebdev <yushanwebdev@gmail.com>
2022-11-13 19:59:59 +0530

    feat: Metro Bundler Update - Configure the rnx kit Cli for bundling

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,18 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const {makeMetroConfig} = require('@rnx-kit/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
+const {
+  DuplicateDependencies,
+} = require('@rnx-kit/metro-plugin-duplicates-checker');
+const {MetroSerializer} = require('@rnx-kit/metro-serializer');
 
-module.exports = {
+module.exports = makeMetroConfig({
+  projectRoot: __dirname,
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+  },
+  serializer: {
+    customSerializer: MetroSerializer([DuplicateDependencies()]),
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -14,4 +21,4 @@ module.exports = {
       },
     }),
   },
-};
+});
